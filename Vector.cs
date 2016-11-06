0ff8d860865ba46900de6e2cc1acd6f7d06105b1
Jacob Hinchsliff <Jshinchsliff@gmail.com>
2016-11-06 13:14:00 -0600

    Add files via upload

---
+++ Vector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeClasses
+{
+    public class Vector
+    {       
+        //properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        //constructors
+        public Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }//FQCTOR
+
+        //Methods
+        public Vector() : base() { }
+    }
+}
